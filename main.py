e498d9cd569e2c6b0540ba433f0c934a80d5cb10
TitanAJ13 <titanaj13@gmail.com>
2025-09-25 23:48:00 -0400

    Fixed and Refactored API code

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, abort, redirect, request, url_for
-from sqlalchemy import create_engine, desc
-from sqlalchemy.orm import sessionmaker
+# from sqlalchemy import create_engine, desc
+# from sqlalchemy.orm import sessionmaker
 from models import Base, Link, Module, Announcement, CalendarItem, FileData, MusicData, Item
 import datetime
 from typing import Any
+from session import MySession, generateSQLSession
 
 def error(message):
     return {
@@ -18,94 +19,7 @@ def success():
 
 app = Flask(__name__)
 
-def generateSQLSession(dbName):
-    engine = create_engine("sqlite:///" + dbName, echo=True)
-    Base.metadata.create_all(bind=engine)
-    Session = sessionmaker(bind=engine)
-    return Session()
-
 sqlSession = generateSQLSession('data.db')
-
-def getLinksJSON():
-    links = sqlSession.query(Link).order_by(Link.position).all()
-    obj = []
-    for link in links:
-        obj.append(link.toJSON())
-    return obj
-
-def getModulesJSON():
-    modules = sqlSession.query(Module).order_by(Module.position).all()
-    obj = []
-    for module in modules:
-        obj.append(module.toJSON())
-    return obj
-
-def getItemsJSON(id):
-    items = sqlSession.query(Item).filter_by(module_id=id).order_by(Item.position).all()
-    obj = []
-    for item in items:
-        obj.append(item.toJSON())
-    return obj
-
-def getAnnouncementsJSON():
-    announcements = sqlSession.query(Announcement).order_by(desc(Announcement.date_posted)).all()
-    obj = []
-    for announcement in announcements:
-        announcement.date_posted = announcement.date_posted.date()
-        obj.append(announcement.toJSON())
-    return obj
-
-def getCalendarItemsJSON():
-    calendarItems = sqlSession.query(CalendarItem).order_by(CalendarItem.target_date).all()
-    obj = []
-    for calendarItem in calendarItems:
-        if (datetime.now() <= calendarItem.target_date and datetime.now() + datetime.timedelta(weeks=1) >= calendarItem.target_date):
-            obj.append(calendarItem.toJSON())
-        else:
-            break
-    return obj
-
-def getModule(position):
-    return sqlSession.query(Module).filter_by(position=position).first()
-
-def getLink(position):
-    return sqlSession.query(Link).filter_by(position=position).first()
-
-def getFile(key):
-    return sqlSession.query(FileData).filter_by(key=key).first()
-
-def getItem(modulePos, itemPos):
-    return sqlSession.query(Item).filter_by(module_id = getModule(modulePos).id, position=itemPos).first()
-
-def moveLink(pos1, pos2):
-    link1 = getLink(pos1)
-    linkList = sqlSession.query(Link).order_by(Link.position).all()
-    linkList.remove(link1)
-    linkList.insert(pos2 - 1, link1)
-    minPos = pos1 if pos1 < pos2 else pos2
-    for i in range(minPos, len(linkList)):
-        linkList[i].position = i+1
-    sqlSession.commit()
-
-def moveModule(pos1, pos2):
-    module1 = getModule(pos1)
-    moduleList = sqlSession.query(Module).order_by(Module.position).all()
-    moduleList.remove(module1)
-    moduleList.insert(pos2 - 1, module1)
-    minPos = pos1 if pos1 < pos2 else pos2
-    for i in range(minPos, len(moduleList)):
-        moduleList[i].position = i+1
-    sqlSession.commit()
-
-def moveItem(modulePos, pos1, pos2):
-    item1 = getItem(modulePos, pos1)
-    itemList = sqlSession.query(Item).filter_by(module_id = item1.module_id).order_by(Item.position).all()
-    itemList.remove(item1)
-    itemList.insert(pos2 - 1, item1)
-    minPos = pos1 if pos1 < pos2 else pos2
-    for i in range(minPos, len(itemList)):
-        itemList[i].position = i+1
-    sqlSession.commit()
 
 def requiredVar(var: dict[str, Any], item: str):
     try:
@@ -125,26 +39,26 @@ def optionalVar(var: dict[str, Any], item: str):
 
 @app.route("/")
 def home():
-    links = getLinksJSON()
-    modules = getModulesJSON()
+    links = sqlSession.getLinksJSON()
+    modules = sqlSession.getModulesJSON()
     for module in modules:
-        module['blocks'] = getItemsJSON(module['id'])
-    announcements = getAnnouncementsJSON()[0:3]
-    calendarItems = getCalendarItemsJSON()
+        module['blocks'] = sqlSession.getItemsJSON(module['id'])
+    announcements = sqlSession.getAnnouncementsJSON()[0:3]
+    calendarItems = sqlSession.getCalendarItemsJSON()
     return render_template("home.html", links=links, modules=modules, announcements=announcements, calendarItems=calendarItems)
 
 
 
 @app.route("/modules/", methods=["GET","POST","PATCH","PUT","DELETE"])
 def modules():
-    moduleList = getModulesJSON()
+    moduleList = sqlSession.getModulesJSON()
     length = len(moduleList)
 
 
     if request.method == "GET":
-        links = getLinksJSON()
+        links = sqlSession.getLinksJSON()
         for module in moduleList:
-            module['blocks'] = getItemsJSON(module['id'])
+            module['blocks'] = sqlSession.getItemsJSON(module['id'])
         return render_template("modules.html", links=links, modules=moduleList)
     
     json = request.json
@@ -158,11 +72,11 @@ def modules():
     if request.method == "POST":
 
         title = requiredVar(json, 'display_name')
+        visibility = requiredVar(json, 'hidden')
         
         try:
-            moduleObj = Module(position = position, display_name = title, hidden = module['hidden'])
-            sqlSession.add(moduleObj)
-            sqlSession.commit()
+            moduleObj = Module(position = position, display_name = title, hidden = visibility)
+            sqlSession.addModule(moduleObj)
             return success()
         except Exception as e:
             abort(500, e)
@@ -171,11 +85,8 @@ def modules():
 
     elif request.method == "DELETE":
         try:
-            moduleObj = getModule(position)
-            id = int(moduleObj.id)
-            sqlSession.delete(moduleObj)
-            sqlSession.query(Item).filter_by(module_id = id).delete()
-            sqlSession.commit()
+            moduleObj = sqlSession.getModule(position)
+            sqlSession.deleteModule(moduleObj)
             return success()
         except Exception as e:
             abort(500, e)
@@ -192,12 +103,12 @@ def modules():
             abort(400, '`changes` must include at least one of `display_name` or `hidden` attributes')
         
         try:
-            moduleObj = getModule(position)
+            moduleObj = sqlSession.getModule(position)
             if (visibility is not None):
                 moduleObj.hidden = visibility
             if (newTitle is not None):
                 moduleObj.display_name = newTitle
-            sqlSession.commit()
+            sqlSession.session.commit()
             return success()
         except Exception as e:
             abort(500, e)
@@ -214,7 +125,7 @@ def modules():
             abort(400, 'Positions must be different')
         
         try:
-            moveModule(position, position2)
+            sqlSession.moveModule(position, position2)
             return success()
         except Exception as e:
             abort(500, e)
@@ -223,7 +134,7 @@ def modules():
 
 @app.route("/links/", methods=["POST","PATCH","DELETE","PUT"])
 def links():
-    links = getLinksJSON()
+    links = sqlSession.getLinksJSON()
     length = len(links)  
     json = request.json
 
@@ -241,8 +152,7 @@ def links():
 
         try:
             linkObj = Link(position = position, display_name = title, url = url, type=type)
-            sqlSession.add(linkObj)
-            sqlSession.commit()
+            sqlSession.addLink(linkObj)
             return success()
         except Exception as e:
             abort(500, e)
@@ -251,9 +161,8 @@ def links():
 
     elif request.method == "DELETE":
         try:
-            linkObj = getLink(position)
-            sqlSession.delete(linkObj)
-            sqlSession.commit()
+            linkObj = sqlSession.getLink(position)
+            sqlSession.deleteLink(linkObj)
             return success()
         except Exception as e:
             abort(500, e)
@@ -271,14 +180,14 @@ def links():
             abort(400, '`changes` must include at least one of `type`, `title`, or `url` attributes')
         
         try:
-            linkObj = getLink(position)
+            linkObj = sqlSession.getLink(position)
             if (title is not None):
                 linkObj.display_name = title
             if (type is not None):
                 linkObj.type = type
             if (url is not None):
                 linkObj.url = url
-            sqlSession.commit()
+            sqlSession.session.commit()
             return success()
         except Exception as e:
             abort(500, e)
@@ -294,7 +203,7 @@ def links():
             abort(400, 'Positions must be different')
         
         try:
-            moveLink(position, position2)
+            sqlSession.moveLink(position, position2)
             return success()
         except Exception as e:
             abort(500, e)
@@ -304,18 +213,21 @@ def links():
 @app.route("/items/", methods=["POST","PATCH","DELETE", "PUT"])
 def items():    
     json = request.json
+    print(json)
 
     modulePos = requiredVar(json, 'moduleposition')
-    mLength = len(getModulesJSON())
+    mLength = len(sqlSession.getModulesJSON())
 
     if (modulePos > mLength):
         abort(400, f'`moduleposition` cannot be larger than {mLength}')
 
-    moduleObj = getModule(modulePos)
-    items = getItemsJSON(moduleObj.id)
+    moduleObj = sqlSession.getModule(modulePos)
+    items = sqlSession.getItemsJSON(moduleObj.id)
     iLength = len(items)
 
     position = requiredVar(json, 'position')
+    if (position is None):
+        position = iLength + 1
     if (position > iLength + 1):
         abort(400, f'`position` cannot be larger than {iLength + 1}')
 
@@ -328,8 +240,7 @@ def items():
 
         try:
             itemObj = Item(position = position, display = title, url = url, type=type, module_id=moduleObj.id, hidden=visibility)
-            sqlSession.add(itemObj)
-            sqlSession.commit()
+            sqlSession.addItem(itemObj)
             return success()
         except Exception as e:
             abort(500, e)
@@ -338,9 +249,8 @@ def items():
 
     elif request.method == "DELETE":
         try:
-            itemObj = getItem(modulePos, position)
-            sqlSession.delete(itemObj)
-            sqlSession.commit()
+            itemObj = sqlSession.getItem(modulePos, position)
+            sqlSession.deleteItem(itemObj)
             return success()
         except Exception as e:
             abort(500, e)
@@ -355,7 +265,7 @@ def items():
         visibility = optionalVar(changes, 'hidden')
 
         try:
-            itemObj = getItem(modulePos, position)
+            itemObj = sqlSession.getItem(modulePos, position)
             if (title is not None):
                 itemObj.display = title
             if (type is not None):
@@ -364,7 +274,7 @@ def items():
                 itemObj.url = url
             if (visibility is not None):
                 itemObj.hidden = visibility
-            sqlSession.commit()
+            sqlSession.session.commit()
             return success()
         except Exception as e:
             abort(500, e)
@@ -380,7 +290,7 @@ def items():
             abort(400, 'Positions must be different')
         
         try:
-            moveItem(modulePos, position, position2)
+            sqlSession.moveItem(modulePos, position, position2)
             return success()
         except Exception as e:
             abort(500, e)
@@ -389,9 +299,9 @@ def items():
 @app.route("/announcements/", methods=["GET","POST","PATCH","DELETE"])
 def announcements():
     if request.method == "GET":
-        announcements = getAnnouncementsJSON()
-        links = getLinksJSON()
-        calendarItems = getCalendarItemsJSON()
+        announcements = sqlSession.getAnnouncementsJSON()
+        links = sqlSession.getLinksJSON()
+        # calendarItems = sqlSession.getCalendarItemsJSON()
         return render_template("announcements.html", links=links, announcements=announcements)
     announcement = request.json
     if request.method == "POST":
@@ -402,8 +312,7 @@ def announcements():
                 date = datetime.datetime.fromisoformat(date)
             print(date, type(date))
             announcementObj = Announcement(author = announcement['author'], title = announcement['title'], date_posted = date, content = announcement['content'])
-            sqlSession.add(announcementObj)
-            sqlSession.commit()
+            sqlSession.addAnnouncement(announcementObj)
             return success()
         except:
             return error('Invalid Announcement Object')
@@ -413,8 +322,7 @@ def announcements():
             id = announcement['id']
             try:
                 announcementObj = sqlSession.query(Announcement).get(id)
-                sqlSession.delete(announcementObj)
-                sqlSession.commit()
+                sqlSession.deleteAnnouncement(announcementObj)
             except:
                 return "Could not delete the announcement"
         except:
@@ -427,11 +335,11 @@ def announcements():
 
 @app.route("/announcement/<id>")
 def announcement(id):
-    announcement = sqlSession.query(Announcement).get(id)
+    announcement = sqlSession.session.query(Announcement).get(id)
     if (announcement is not None):
         announcement = announcement.toJSON()
         announcement['initial'] = announcement['author'][0].upper()
-        return render_template("announcement.html", announcement=announcement, links=getLinksJSON())
+        return render_template("announcement.html", announcement=announcement, links=sqlSession.getLinksJSON())
     else:
         return redirect(url_for("announcements"))
 
@@ -448,8 +356,7 @@ def files():
 
         try:
             fileObj = FileData(key = key, url = url, display_name = title)
-            sqlSession.add(fileObj)
-            sqlSession.commit()
+            sqlSession.addFile(fileObj)
             return success()
         except Exception as e:
             abort(500, e)
@@ -458,9 +365,8 @@ def files():
 
     elif request.method == "DELETE":
         try:
-            fileObj = sqlSession.query(FileData).get(key)
-            sqlSession.delete(fileObj)
-            sqlSession.commit()
+            fileObj = sqlSession.getFile(key)
+            sqlSession.deleteFile(fileObj)
             return success()
         except Exception as e:
             abort(500, e)
@@ -475,26 +381,26 @@ def files():
         url = optionalVar(changes, 'url')
 
         try:
-            file = getFile(key)
+            file = sqlSession.getFile(key)
             if (filename is not None):
                 file.display_name = filename
             if (path is not None):
                 file.key = path
             if (url is not None):
                 file.url = url
-            sqlSession.commit()
+            sqlSession.session.commit()
         except Exception as e:
             abort(500, e)
 
 
 @app.route("/file/<path:key>")
 def file(key):
-    data = sqlSession.query(FileData).get(key)
+    data = sqlSession.getFile(key)
     if (data is None):
-        return render_template("file.html", header="Unnamed File", url=key)
+        return render_template("file.html", header="Unnamed File", url=key, links=sqlSession.getLinksJSON())
     else:
         data = data.toJSON()
-        return render_template("file.html", header= data['display_name'], url=data['url'])
+        return render_template("file.html", header= data['display_name'], url=data['url'], links=sqlSession.getLinksJSON())
     
 
 
@@ -511,8 +417,7 @@ def musicdata():
 
         try:
             musicObj = MusicData(key = path, url = url, display_name = filename)
-            sqlSession.add(musicObj)
-            sqlSession.commit()
+            sqlSession.addMusic(musicObj)
             return success()
         except Exception as e:
             abort(500, e)
@@ -521,9 +426,8 @@ def musicdata():
 
     elif request.method == "DELETE":
         try:
-            musicObj = sqlSession.query(MusicData).get(path)
-            sqlSession.delete(musicObj)
-            sqlSession.commit()
+            musicObj = sqlSession.getMusic(path)
+            sqlSession.deleteMusic(musicObj)
             return success()
         except Exception as e:
             abort(500, e)
@@ -541,30 +445,30 @@ def musicdata():
             abort(400, 'At least one of `new_path`, `filename`, or `url` must be defined')
 
         try:
-            musicObj = sqlSession.query(MusicData).get(path)
+            musicObj = sqlSession.getMusic(path)
             if (new_path is not None):
                 musicObj.key = new_path
             if (filename is not None):
                 musicObj.display_name = filename
             if (url is not None):
                 musicObj.url = url
-            sqlSession.commit()
+            sqlSession.session.commit()
         except Exception as e:
             abort(500, e)
 
 
 @app.route("/music/<path:key>")
 def music(key):
-    data = sqlSession.query(MusicData).get(key)
+    data = sqlSession.getMusic(key)
     if (data is None):
-        return render_template("music.html", header="Unnamed Sheetmusic", url=key)
+        return render_template("music.html", header="Unnamed Sheetmusic", url=key, links=sqlSession.getLinksJSON())
     else:
         data = data.toJSON()
-        return render_template("music.html", header=data['display_name'], url=data['url'])
+        return render_template("music.html", header=data['display_name'], url=data['url'], links=sqlSession.getLinksJSON())
 
 @app.route("/calendar/")
 def calendar():
-    links = getLinksJSON()
+    links = sqlSession.getLinksJSON()
     return render_template("calendar.html", links=links)
 
 @app.route("/page/<id>")
---
+++ session.py
@@ -0,0 +1,174 @@
+from sqlalchemy import create_engine, desc
+from sqlalchemy.orm import sessionmaker, Session
+from models import Base, Link, Module, Announcement, CalendarItem, FileData, MusicData, Item
+import datetime
+from typing import Any
+
+class MySession():
+    def __init__(self, session: Session):
+        self.session = session
+
+    def getLinksJSON(self) -> list[dict[str, Any]]:
+        """Returns all `Link` objects in the database parsed as Python `dict` objects"""
+        links = self.session.query(Link).order_by(Link.position).all()
+        obj = []
+        for link in links:
+            obj.append(link.toJSON())
+        return obj
+
+    def getModulesJSON(self) -> list[dict[str, Any]]:
+        """Returns all `Module` objects in the database parsed as Python `dict` objects"""
+        modules = self.session.query(Module).order_by(Module.position).all()
+        obj = []
+        for module in modules:
+            obj.append(module.toJSON())
+        return obj
+
+    def getItemsJSON(self, id) -> list[dict[str, Any]]:
+        items = self.session.query(Item).filter_by(module_id=id).order_by(Item.position).all()
+        obj = []
+        for item in items:
+            obj.append(item.toJSON())
+        return obj
+
+    def getAnnouncementsJSON(self) -> list[dict[str, Any]]:
+        announcements = self.session.query(Announcement).order_by(desc(Announcement.date_posted)).all()
+        obj = []
+        for announcement in announcements:
+            announcement.date_posted = announcement.date_posted.date()
+            obj.append(announcement.toJSON())
+        return obj
+
+    def getCalendarItemsJSON(self) -> list[dict[str, Any]]:
+        calendarItems = self.session.query(CalendarItem).order_by(CalendarItem.target_date).all()
+        obj = []
+        for calendarItem in calendarItems:
+            if (datetime.now() <= calendarItem.target_date and datetime.now() + datetime.timedelta(weeks=1) >= calendarItem.target_date):
+                obj.append(calendarItem.toJSON())
+            else:
+                break
+        return obj
+
+    def getModule(self, position) -> Module | None:
+        return self.session.query(Module).filter_by(position=position).first()
+
+    def getLink(self, position) -> Link | None:
+        return self.session.query(Link).filter_by(position=position).first()
+
+    def getFile(self, key) -> FileData | None:
+        return self.session.query(FileData).filter_by(key=key).first()
+    
+    def getMusic(self, key) -> MusicData | None:
+        return self.session.query(MusicData).filter_by(key=key).first()
+
+    def getItem(self, modulePos, itemPos) -> Item | None:
+        return self.session.query(Item).filter_by(module_id = self.getModule(modulePos).id, position=itemPos).first()
+
+    def moveLink(self, pos1, pos2):
+        link1 = self.getLink(pos1)
+        linkList = self.session.query(Link).order_by(Link.position).all()
+        linkList.remove(link1)
+        linkList.insert(pos2 - 1, link1)
+        minPos = pos1 if pos1 < pos2 else pos2
+        print(f"start: {minPos}; stop: {len(linkList)}")
+        for i in range(minPos - 1, len(linkList)):
+            linkList[i].position = i + 1
+        self.session.commit()
+
+    def moveModule(self, pos1, pos2):
+        module1 = self.getModule(pos1)
+        moduleList = self.session.query(Module).order_by(Module.position).all()
+        moduleList.remove(module1)
+        moduleList.insert(pos2 - 1, module1)
+        minPos = pos1 if pos1 < pos2 else pos2
+        for i in range(minPos - 1, len(moduleList)):
+            moduleList[i].position = i+1
+        self.session.commit()
+
+    def moveItem(self, modulePos, pos1, pos2):
+        item1 = self.getItem(modulePos, pos1)
+        itemList = self.session.query(Item).filter_by(module_id = item1.module_id).order_by(Item.position).all()
+        itemList.remove(item1)
+        itemList.insert(pos2 - 1, item1)
+        minPos = pos1 if pos1 < pos2 else pos2
+        for i in range(minPos - 1, len(itemList)):
+            itemList[i].position = i+1
+        self.session.commit()
+
+    def addModule(self, module: Module):
+        all = self.session.query(Module).order_by(Module.position).all()
+        self.session.add(module)
+        all.insert(module.position-1, module)
+        for i in range(module.position, len(all)):
+            all[i].position = i + 1
+        self.session.commit()
+
+    def addLink(self, link: Link):
+        all = self.session.query(Link).order_by(Link.position).all()
+        self.session.add(link)
+        all.insert(link.position-1, link)
+        for i in range(link.position, len(all)):
+            all[i].position = i + 1
+        self.session.commit()
+
+    def addItem(self, item: Item):
+        all = self.session.query(Item).filter_by(module_id = item.module_id).order_by(Item.position).all()
+        self.session.add(item)
+        all.insert(item.position-1, item)
+        for i in range(item.position, len(all)):
+            all[i].position = i + 1
+        self.session.commit()
+
+    def addFile(self, file: FileData):
+        self.session.add(file)
+        self.session.commit()
+
+    def addMusic(self, music: MusicData):
+        self.session.add(music)
+        self.session.commit()
+
+    def addAnnouncement(self, announcement: Announcement):
+        self.session.add(announcement)
+        self.session.commit()
+
+    def deleteModule(self, module: Module):
+        all = self.session.query(Module).order_by(Module.position).all()
+        all.remove(module)
+        for i in range(module.position - 1, len(all)):
+            all[i].position = i + 1
+        self.session.delete(module)
+        self.session.commit()
+
+    def deleteLink(self, link: Link):
+        all = self.session.query(Link).order_by(Link.position).all()
+        all.remove(link)
+        for i in range(link.position - 1, len(all)):
+            all[i].position = i + 1
+        self.session.delete(link)
+        self.session.commit()
+
+    def deleteItem(self, item: Item):
+        all = self.session.query(Item).filter_by(module_id = item.module_id).order_by(Item.position).all()
+        all.remove(item)
+        for i in range(item.position - 1, len(all)):
+            all[i].position = i + 1
+        self.session.delete(item)
+        self.session.commit()
+
+    def deleteFile(self, file: FileData):
+        self.session.delete(file)
+        self.session.commit()
+
+    def deleteMusic(self, music: MusicData):
+        self.session.delete(music)
+        self.session.commit()
+
+    def deleteAnnouncement(self, announcement: Announcement):
+        self.session.delete(announcement)
+        self.session.commit()
+
+def generateSQLSession(dbName) -> MySession:
+    engine = create_engine("sqlite:///" + dbName)
+    Base.metadata.create_all(bind=engine)
+    Session = sessionmaker(bind=engine)
+    return MySession(Session())
